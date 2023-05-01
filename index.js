759ed221f3952b75622cd32b2e96c637a481b3c0
Robbani571 <gr77rubel@gmail.com>
2023-05-02 00:33:20 +0600

    food data added and setup completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const foodData = require('./data/foodData.json')
+
+app.get('/', (req, res) => {
+    res.send('The Eaters server is runnig')
+})
+
+app.get('/foods', (req, res) => {
+    res.send(foodData)
+})
+
+app.get('foods/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedFood = foodData.find(f => f._id === id);
+    res.send(selectedFood)
+})
+
+app.listen(port, () => {
+    console.log('The eaters server is runnign on port:', port)
+})
