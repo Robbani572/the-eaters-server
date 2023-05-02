6d31579c0aff32e47af9171a93c231a3d158cdd0
Robbani571 <gr77rubel@gmail.com>
2023-05-02 08:45:19 +0600

    load data with id problem fixed

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const foodData = require('./data/foodData.json')
+
 app.use(cors())
 
-const foodData = require('./data/foodData.json')
 
 app.get('/', (req, res) => {
     res.send('The Eaters server is runnig')
@@ -15,7 +16,7 @@ app.get('/foods', (req, res) => {
     res.send(foodData)
 })
 
-app.get('foods/:id', (req, res) => {
+app.get('/foods/:id', (req, res) => {
     const id = req.params.id;
     const selectedFood = foodData.find(f => f._id === id);
     res.send(selectedFood)
